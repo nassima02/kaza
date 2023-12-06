e25403567dbfc439c6ca058c238297bfce0ff7c5
nassima bression <benasima@yahoo.fr>
2023-12-06 10:29:55 +0100

    feat[Banner]: création du composant Banner

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Template from '../components/Template'
+import Card from '../components/Card'
+import Banner from '../components/Banner'
 function About() {
     return (
         <Template>
-            <h2>Ici va être insérer le contenu de la page a propos</h2>
+            <div className="home">
+                <Banner
+                    title=""
+                    src="/bannerApropos.png"
+                    alt="Image de bannière représentant une vue sur les montagnes"
+                />
+
+                <section className="gallery"></section>
+            </div>
         </Template>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import Template from '../components/Template'
+import Card from '../components/Card'
+import Banner from '../components/Banner'
 
 function Home() {
+    const galleryData = [
+        { id: 1, title: 'Carte 1', imageUrl: '/image1.jpg' },
+        { id: 2, title: 'Carte 2', imageUrl: '/image2.jpg' },
+        { id: 3, title: 'Carte 3', imageUrl: '/image3.jpg' },
+        { id: 4, title: 'Carte 4', imageUrl: '/image4.jpg' },
+        { id: 5, title: 'Carte 5', imageUrl: '/image5.jpg' },
+        { id: 6, title: 'Carte 6', imageUrl: '/image6.jpg' },
+    ]
+
     return (
         <Template>
-            {/* Contenu spécifique à cette page */}
-            <h2>Ici va être insérer le contenu de ma page</h2>
+            <div className="home">
+                <Banner
+                    title="Chez vous, partout et ailleurs"
+                    src="/banner.png"
+                    alt="Image de bannière représentant une vue sur les montagnes"
+                />
+
+                <section className="gallery">
+                    {/* Utiliser map pour créer les composants de carte pour chaque élément de galleryData */}
+                    {galleryData.map((card) => (
+                        <Card
+                            key={card.id}
+                            title={card.title}
+                            imageUrl={card.imageUrl}
+                        />
+                    ))}
+                </section>
+            </div>
         </Template>
     )
 }
-
 export default Home
